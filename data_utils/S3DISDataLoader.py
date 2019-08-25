3a478d5241bfa996a335ddaa1e197ca7b442ac91
James <jmy9605@gmail.com>
2019-08-25 18:15:11 +0800

    mod

--- data_utils/S3DISDataLoader.py
+++ data_utils/S3DISDataLoader.py
@@ -28,11 +28,13 @@ def recognize_all_data(root, test_area = 5):
     room_filelist = [line.rstrip() for line in open(os.path.join(root, 'room_filelist.txt'))]
     data_batch_list = []
     label_batch_list = []
+
     for h5_filename in ALL_FILES:
         fn = os.path.join(os.path.abspath(os.path.join(root,'..')),h5_filename)
         data_batch, label_batch = loadDataFile(fn)
         data_batch_list.append(data_batch)
         label_batch_list.append(label_batch)
+        
     data_batches = np.concatenate(data_batch_list, 0)
     label_batches = np.concatenate(label_batch_list, 0)
 
@@ -45,9 +47,9 @@ def recognize_all_data(root, test_area = 5):
         else:
             train_idxs.append(i)
 
-    train_data = data_batches[train_idxs, ...]
+    train_data = data_batches[train_idxs]
     train_label = label_batches[train_idxs]
-    test_data = data_batches[test_idxs, ...]
+    test_data = data_batches[test_idxs]
     test_label = label_batches[test_idxs]
     return train_data,train_label,test_data,test_label
 
