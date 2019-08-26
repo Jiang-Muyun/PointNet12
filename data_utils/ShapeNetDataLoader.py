9c4dc0bc539e1dffed89f86cd2971affc732018a
James <jmy9605@gmail.com>
2019-08-26 15:06:28 +0800

    mod

--- data_utils/ShapeNetDataLoader.py
+++ data_utils/ShapeNetDataLoader.py
@@ -18,32 +18,34 @@ def load_data(root):
     if not os.path.exists(fn_cache):
         print_info('Indexing Files...')
         fns_full = []
-        for line in open(os.path.join(root, 'synsetoffset2category.txt'), 'r'):
-            name,wordnet_id = line.strip().split()
-            pt_folder = os.path.join(root, wordnet_id)
-            for fn in os.listdir(pt_folder):
-                fns_full.append(os.path.join(pt_folder, fn))
-
-        print_info('Building cache...')
-        
         fp_h5 = h5py.File(fn_cache,"w")
         pt_group = fp_h5.create_group("pt")
 
-        cache = {}
-        for i,fn_full in tqdm(enumerate(fns_full), total=len(fns_full), smoothing=0.9):
-            token = fn_full.split('/')[-1].split('.')[0]
-            pts = np.loadtxt(fn_full).astype(np.float32)
-            if token in pt_group.keys():
-                print_err('Token exists',token)
-            else:
-                pt_group[token] = pts
+        for line in open(os.path.join(root, 'synsetoffset2category.txt'), 'r'):
+            name,wordnet_id = line.strip().split()
+            pt_folder = os.path.join(root, wordnet_id)
+            print_info('Building',name, wordnet_id)
+            for fn in tqdm(os.listdir(pt_folder)):
+                token = fn.split('.')[0]
+                fn_full = os.path.join(pt_folder, fn)
+                pts = np.loadtxt(fn_full).astype(np.float32)
+
+                if token in pt_group.keys():
+                    print_err('Token exists',token)
+                else:
+                    pt_group[token] = pts
+
+        print_info('Building cache...')
         fp_h5.close()
-    else:
-        print_info('Loading from cahce...')
-        fp_h5 = h5py.File(fn_cache, 'r')
-        print_info(fp_h5['pt'].keys())
 
     exit()
+
+    print_info('Loading from cache...')
+    fp_h5 = h5py.File(fn_cache, 'r')
+    pt = fp_h5['pt']
+    cache = {}
+    for token in pt.keys():
+        cache[token] = pt[token]
     return cache
 
 
@@ -114,7 +116,7 @@ class PartNormalDataset(Dataset):
                 self.datapath.append((item, fn))
 
         self.classes = dict(zip(self.category, range(len(self.category))))
-        print_kv('classes',self.classes)
+        print_kv('classes',self.classes.keys())
 
         self.seg_classes = {'Earphone': [16, 17, 18], 'Motorbike': [30, 31, 32, 33, 34, 35], 'Rocket': [41, 42, 43],
                             'Car': [8, 9, 10, 11], 'Laptop': [28, 29], 'Cap': [6, 7], 'Skateboard': [44, 45, 46],
@@ -129,32 +131,36 @@ class PartNormalDataset(Dataset):
     def __getitem__(self, index):
         token = self.datapath[index][1].split('/')[-1].split('.')[0]
         if token in self.full_cache.keys():
-            print_info('full cache hit',token)
-            fn = self.datapath[index]
-            cat = self.datapath[index][0]
-            classi = self.classes[cat]
-            classi = np.array([classi]).astype(np.int32)
+            category = self.datapath[index][0]
+            cls_id = self.classes[category]
+            cls_id = np.array([cls_id]).astype(np.int32)
             data = self.full_cache[token]
-            print_kv('data',data.shape)
             point_set = data[:, 0:3]
             normal = data[:, 3:6]
             seg = data[:, -1].astype(np.int32)
+            point_set1, normal1, seg1, cls_id1 = point_set, normal, seg, cls_id
         else:
-            if index in self.cache:
-                print_info('cache hit',token)
-                point_set, normal, seg, classi = self.cache[index]
-            else:
-                print_err('cache miss',token)
-                fn = self.datapath[index]
-                cat = self.datapath[index][0]
-                classi = self.classes[cat]
-                classi = np.array([classi]).astype(np.int32)
-                data = np.loadtxt(fn[1]).astype(np.float32)
-                point_set = data[:, 0:3]
-                normal = data[:, 3:6]
-                seg = data[:, -1].astype(np.int32)
-                self.cache[index] = (point_set, normal, seg, classi)
+            print_err('full cache miss')
+
+        if index in self.cache.keys():
+            print_info('cache hit',token)
+            point_set, normal, seg, cls_id = self.cache[index]
+        else:
+            category = self.datapath[index][0]
+            fn = self.datapath[index][1]
+            cls_id = np.array([self.classes[category]]).astype(np.int32)
+            data = np.loadtxt(fn).astype(np.float32)
+            point_set = data[:, 0:3]
+            normal = data[:, 3:6]
+            seg = data[:, -1].astype(np.int32)
+            self.cache[index] = (point_set, normal, seg, cls_id)
         
+        if (point_set1-point_set).mean() != 0:
+            print_err('point_set not equal',blue((point_set1-point_set).mean()))
+        if (normal1-normal).mean() != 0:print_err('normal not equal')
+        if (seg1-seg).mean() != 0:print_err('seg not equal')
+        if (cls_id1-cls_id).mean() != 0:print_err('cls_id not equal')
+
         if self.normalize:
             point_set = pc_normalize(point_set)
             
@@ -167,8 +173,7 @@ class PartNormalDataset(Dataset):
         point_set = point_set[choice, :]
         seg = seg[choice]
         normal = normal[choice, :]
-        return point_set,classi, seg, normal
-
+        return point_set,cls_id, seg, normal
 
     def __len__(self):
         return len(self.datapath)
@@ -233,17 +238,17 @@ class PartNormalDataset(Dataset):
 #     def __getitem__(self, index):
 #         print_err('cached',len(self.cache.keys()), index)
 #         if index in self.cache:
-#             point_set, normal, seg, classi = self.cache[index]
+#             point_set, normal, seg, cls_id = self.cache[index]
 #         else:
 #             fn = self.datapath[index]
-#             cat = self.datapath[index][0]
-#             classi = self.classes[cat]
-#             classi = np.array([classi]).astype(np.int32)
+#             category = self.datapath[index][0]
+#             cls_id = self.classes[category]
+#             cls_id = np.array([cls_id]).astype(np.int32)
 #             data = np.loadtxt(fn[1]).astype(np.float32)
 #             point_set = data[:, 0:3]
 #             normal = data[:, 3:6]
 #             seg = data[:, -1].astype(np.int32)
-#             self.cache[index] = (point_set, normal, seg, classi)
+#             self.cache[index] = (point_set, normal, seg, cls_id)
         
 #         if self.normalize:
 #             point_set = pc_normalize(point_set)
@@ -257,7 +262,7 @@ class PartNormalDataset(Dataset):
 #         point_set = point_set[choice, :]
 #         seg = seg[choice]
 #         normal = normal[choice, :]
-#         return point_set,classi, seg, normal
+#         return point_set,cls_id, seg, normal
 
 
 #     def __len__(self):
