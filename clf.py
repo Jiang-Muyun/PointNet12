f18a53dfc773b5fec56cac2e6bdca0c3afbe20c2
James <jmy9605@gmail.com>
2019-08-27 11:25:01 +0800

    add kitti

--- clf.py
+++ clf.py
@@ -102,7 +102,6 @@ def train(args):
     if len(device_ids) >= 2:
         torch.backends.cudnn.benchmark = True
         model.cuda(device_ids[0])
-        print_kv('device_ids')
         model = torch.nn.DataParallel(model, device_ids=device_ids)
         print_info('Using multi GPU:',device_ids)
     else:
